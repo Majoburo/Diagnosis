cab007f9f955edce3cf036d7c43c2ab9021fad3c
Majoburo <majoburo@gmail.com>
2019-02-28 01:15:19 -0600

    HETDEX in healpy pixels

---
+++ email_ip.py
@@ -0,0 +1,28 @@
+import smtplib
+
+
+username = 'virusgwhet@gmail.com' 
+password = 'xXYk4S8byXvG88u'
+numbers = [('6015295332','att'),('5125763501','tmobile')]
+emails = ['majoburo@gmail.com','rjrosati@utexas.edu']
+carriers = {'tmobile': '@tmomail.net', 'att': '@txt.att.net'}
+            
+def SendText(content,numbers=numbers,emails=emails):
+    try:
+        mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+        mailserver.ehlo()
+        mailserver.starttls()
+        mailserver.ehlo()
+        mailserver.login(username, password)
+        for number,carrier in numbers:
+            mailserver.sendmail(username, number + carriers[carrier], content)
+            print("Sent {} to {}.".format(content,number))
+        contentsub = 'Subject: {}\n\n{}'.format('GW alert', content)
+        for em in emails:
+            
+            mailserver.sendmail(username, em, contentsub)
+            print("Sent {} to {}.".format(content,em))
+        mailserver.close()
+    except:
+        print('Error sending email!')
+
