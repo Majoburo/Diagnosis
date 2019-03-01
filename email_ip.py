98cb00c134477b16f70838ddb9a3a6cdbeae8953
Majoburo <majoburo@gmail.com>
2019-03-01 16:22:22 -0600

    remove confidential info

--- email_ip.py
+++ email_ip.py
@@ -1,26 +1,28 @@
 import smtplib
+import private as pr
 
 
-username = 'virusgwhet@gmail.com' 
-password = 'xXYk4S8byXvG88u'
 carriers = {'tmobile': '@tmomail.net', 'att': '@txt.att.net'}
-            
-def SendText(content,numbers=numbers,emails=emails):
+def SendText(content,numbers=pr.numbers,emails=pr.emails):
     try:
         mailserver = smtplib.SMTP('smtp.gmail.com', 587)
         mailserver.ehlo()
         mailserver.starttls()
         mailserver.ehlo()
-        mailserver.login(username, password)
+        mailserver.login(pr.username, pr.password)
         for number,carrier in numbers:
-            mailserver.sendmail(username, number + carriers[carrier], content)
+            mailserver.sendmail(pr.username, number + carriers[carrier], content)
             print("Sent {} to {}.".format(content,number))
         contentsub = 'Subject: {}\n\n{}'.format('GW alert', content)
         for em in emails:
-            
-            mailserver.sendmail(username, em, contentsub)
+            mailserver.sendmail(pr.username, em, contentsub)
             print("Sent {} to {}.".format(content,em))
         mailserver.close()
     except:
         print('Error sending email!')
 
+def main():
+    SendText('TEST',numbers=[],emails=['majoburo@gmail.com'])
+
+if __name__=='__main__':
+    main()
