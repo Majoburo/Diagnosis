e1bec0ab2bc517787beb9e29d7efec694489ca52
Majoburo <majoburo@gmail.com>
2019-03-08 00:25:14 -0600

    Final fix to time till event. Sigh...

--- prob_obs.py
+++ prob_obs.py
@@ -128,8 +128,6 @@ def prob_observable(m, header, time, plot = False):
         #if the region doesn't intersect HET at all
         if len(np.intersect1d(p90i,hetfullpix)) == 0:
             return 0 , 0 , -99
-        import pdb
-        pdb.set_trace()
         hetedge = np.loadtxt('hetedge.dat')
         hetedgef = lambda x: np.interp(x,hetedge[:,1],hetedge[:,0])
         y = theta90HET*180/np.pi #DEC SKYMAP
