6e734237bda4d95917b2ec9e110b73924cf0e31f
majoburo <majoburo@grad14mb.as.utexas.edu>
2019-05-24 03:03:10 -0500

    rerun cronjob script

---
+++ check_script_rerun.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#CMD="source /Users/majoburo/.bash_profile; tmux new-session -d -s Diagnosis \"source /Users/majoburo/.bash_profile; cd work/Diagnosis/; python diagnose.py recipients.py \" "
+CMD="source /Users/majoburo/.bash_profile; cd work/Diagnosis/; nohup python diagnose.py recipients.py 2>&1 >> diagnose.stdout & "
+SEARCHSTR="python diagnose.py" # should be start of last part of command
+
+# run pgrep to see if command exists
+pidcmd="pgrep -f \"${SEARCHSTR}\" "
+eval $pidcmd
+status=$?
+if [ $status -ne 0 ] ; then
+    echo "Script dead, rerunning"
+    echo $CMD
+    eval $CMD
+else
+    echo "Running."
+fi
